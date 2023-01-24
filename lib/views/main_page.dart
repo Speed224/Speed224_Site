37263233d94e80e50133729700f35b49e9fa7a16
Speed224 <nicola.cremonesi.2000@gmail.com>
2023-01-24 21:05:49 +0100

    removed sidebar and changed things

--- lib/views/main_page.dart
+++ lib/views/main_page.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
 import 'package:easy_localization/easy_localization.dart';
-import 'package:speed224_site/components/side_bar.dart';
+import 'package:speed224_site/components/custom_page_view.dart';
+import 'package:speed224_site/old/old_coll_side_bar.dart';
 import 'package:speed224_site/views/collapse_bar.dart';
 
 class MainPage extends StatelessWidget {
   final ThemeData theme;
-  MainPage({super.key, required this.theme});
+  const MainPage({super.key, required this.theme});
 
   @override
   Widget build(BuildContext context) {
+    const AssetImage _avatarImg = AssetImage('assets/Foto.png');
+
+    final PageController _controller = PageController(viewportFraction: 1);
     return MaterialApp(
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
@@ -18,14 +22,31 @@ class MainPage extends StatelessWidget {
       debugShowCheckedModeBanner: true,
       theme: theme,
       home: Scaffold(
-        body: SideBar(),
+        body: CustomPageView(controller: _controller, pageList: _generatePages()),
       ),
     );
   }
 }
-/*
-const Text(
-              "Hello World",
-              style: TextStyle(fontSize: 72),
-            )
-            */
+
+List<Widget> _generatePages() {
+  return <Widget>[
+    const Center(
+      child: Text('First Page'),
+    ),
+    Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Second Page'),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("Button"),
+          ),
+        ],
+      ),
+    ),
+    Center(
+      child: Text('Third Page'),
+    ),
+  ];
+}
